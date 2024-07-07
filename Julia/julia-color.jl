019a6258527b457e81138bb0fe6992b0c7f81f39
David <8893118+macromore@users.noreply.github.com>
2024-07-07 17:13:59 -0400

    Initial commit

---
+++ Julia/julia-color.jl
@@ -0,0 +1,43 @@
+using Plots; pythonplot()
+a = exp(im*(1+sqrt(5))/2)
+R = 4
+maxIter = 1000
+xMin = -1.5
+xMax = 1.5
+yMin = -2
+yMax = 1
+step = 0.001
+xNum = length(xMin:step:xMax)
+yNum = length(yMin:step:yMax)
+juliaColor = ones(xNum,yNum)
+juliaSet = []
+let offSet = 0
+x = 1
+for j in xMin:step:xMax
+    y = 1
+    for k in yMin:step:yMax
+        p0 = j + im*k
+        p = p0
+        p_iter = 0
+        for p_iter in 1:maxIter
+            p = p^2 + a*p
+            if abs(p) > R
+                juliaColor[x,y] = p_iter
+                break
+            end
+        end
+        # if p_iter == maxIter
+        #     juliaColor[x,y] = 1 + offSet
+        # end
+        # if y == 1 && x == 1 && p_iter > 0
+        #     offSet = p_iter - 1
+        # elseif y == 1 && x == 1
+        #     offSet = 0
+        # end
+        y += 1
+    end
+    x += 1
+end
+#contourf(juliaColor)
+contour(log.(juliaColor),fill=true)
+end
